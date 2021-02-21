33fdba7c0cc5750889a433e939086c210202a8e5
Tom Konidas <tomkonidas@protonmail.ch>
2021-02-20 22:29:01 -0500

    11ty base

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = (config) => {
+  config.addPassthroughCopy({ './src/assets': '.' });
+
+  return {
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist',
+      includes: 'includes',
+      layouts: 'layouts',
+      data: 'data',
+    },
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+layout: 'home.njk'
+intro:
+  summary: 'Let us help you create the perfect campaign with our multi-faceted team of talented creatives.'
+  image: '/images/hero.jpg'
+  imageAlt: 'Our creative workspace'
+---
+
